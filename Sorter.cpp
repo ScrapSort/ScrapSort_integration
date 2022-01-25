290754c00c8b4df4ac4da701432d8a5cb8652946
Tyler Ekaireb <tylerekaireb@gmail.com>
2022-01-25 12:18:09 -0800

    Fixed/optimized add_item() function

--- Sorter.cpp
+++ Sorter.cpp
@@ -2,9 +2,11 @@
 #include "Sorter.h"
 
 void Sorter::add_item(int type) {
-  for (int i = 0; i < NUM_TYPES; i++) {
-    queues[i].push(i == type);
+  for (int i = 0; i < type; i++) {
+    queues[i].push(false);
   }
+
+  queues[type].push(true);
 }
 
 bool Sorter::detected_item(int flipper) {
